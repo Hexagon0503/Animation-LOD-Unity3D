8c93b84d3bd77148a935eeabc000bd4723eea153
Mordan Lean <realitystudio12@gmail.com>
2022-11-24 17:13:40 +0900

    Culling Update

--- Assets/Content/Scripts/AnimatorLODGroup.cs
+++ Assets/Content/Scripts/AnimatorLODGroup.cs
@@ -21,6 +21,11 @@ public class AnimatorLODGroup : MonoBehaviour
     /// 
     /// </summary>
     private LOD[] allLods;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private int lodIndex = -1;
     #endregion
 
     #region UNITY METHODS
@@ -37,7 +42,9 @@ public class AnimatorLODGroup : MonoBehaviour
     /// </summary>
     private void LateUpdate()
     {
+        lodIndex = GetActiveLODIndex();
         animatorLOD.animFPS = GetAnimFPS();
+        animatorLOD.IsCulled = !IsVisible();
     }
     #endregion
 
@@ -46,32 +53,46 @@ public class AnimatorLODGroup : MonoBehaviour
     /// 
     /// </summary>
     /// <returns></returns>
-    public int GetAnimFPS()
+    private int GetAnimFPS()
     {
-        return profile.GetLODData(GetActiveLODIndex()).animFrame;
+        if (lodIndex == -1)
+        {
+            return 0;
+        }
+        return profile.GetLODData(lodIndex).animFrame;
     }
 
     /// <summary>
     /// 
     /// </summary>
     /// <returns></returns>
+    private bool IsVisible()
+    {
+        return lodIndex != -1;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private int i_lod, i_renderers;
     private int GetActiveLODIndex()
     {
         if(allLods == null)
         {
             allLods = lodGroup.GetLODs();
         }
-        for (int i = 0; i < allLods.Length; i++)
+        for (i_lod = 0; i_lod < allLods.Length; i_lod++)
         {
-            for (int e = 0; e < allLods[i].renderers.Length; e++)
+            for (i_renderers = 0; i_renderers < allLods[i_lod].renderers.Length; i_renderers++)
             {
-                if(allLods[i].renderers[e].isVisible)
+                if(allLods[i_lod].renderers[i_renderers].isVisible)
                 {
-                    return i;
+                    return i_lod;
                 }
             }
         }
-        return allLods.Length - 1;
+        return -1;
     }
     #endregion
 
